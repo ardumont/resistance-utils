9258843132af0d9d5d4bfbbb72425bc6f9a6fc55
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-06-08 09:15:08 +0200

    Added lazytest dep.

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [midje "1.4.0"]
-])
+                 [com.intelie/lazytest
+                  "1.0.0-SNAPSHOT"
+                  :exclusions
+                  [swank-clojure]]])
